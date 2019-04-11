eb48e4ea2b3cc9cdcb948305e9dbfa8415916e22
Ahmad Nurus S <prksu.sh@gmail.com>
2019-04-11 23:21:43 +0700

    Using pip-tools for dependencies management

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md') as f:
 
 setup(
     name='subtr',
-    version='0.0.3',
+    version='0.0.4',
     description='Subtitle Translator',
     long_description=README,
     long_description_content_type="text/markdown",
@@ -23,4 +23,8 @@ setup(
             'subtr = subtr.__main__:main'
         ]
     },
+    install_requires=[
+        'google-cloud-translate==1.4.0',
+        'tqdm==4.31.1'
+    ]
 )
